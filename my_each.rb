ecb2b385accd738bc838e2c6b16a8f02d8e4675f
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-06-21 21:31:38 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,9 @@
-def my_each(collection)# put argument(s) here
-  my_each(collection) do |i|
-  puts i
-end# code here
+def my_each(collection)
+
+ i=0
+ while i < collection.length
+ yield(collection[i])
+ i+=1
+ end
+ collection
 end
